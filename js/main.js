c9a9f0dfc962fb3b0a9c85ab0c9aafcc8db7a947
Miguel0o0 <127349178+Miguel0o0@users.noreply.github.com>
2023-05-13 22:20:31 -0400

    pase la pagina a otro pc

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function login() {
+    var user, password
+    user = document. getElementById("usuario").value;
+    password = document. getElementById("contrasena").value;
+
+    if(user == "prueba" && password == "1234"){
+        window.location = "index.html";
+    }
+    else{
+        alert("Datos incorrectos")
+    }
+} 
+
