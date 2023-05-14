40748309f93e032109d8b7515855f6e441f366d0
Miguel0o0 <127349178+Miguel0o0@users.noreply.github.com>
2023-05-14 01:29:01 -0400

    no me funciona el codigo:((((

--- js/carrito.js
+++ js/carrito.js
@@ -1,3 +1,21 @@
 var carritoVisible = false 
 
-if(Document.readyState=='loading')
+if(Document.readyState=='loading'){
+    document.addEventListener('DOMContentLoaded,ready')
+}
+else{
+    ready();
+}
+
+function ready(){
+    var botonesEliminar = document.getElementsByClassName('btn-eliminar');
+    for(vari=0; i < botonesEliminar.length;i++){
+        var buttom = botonesEliminar[i]
+        buttom.addEventListener('click', eliminarItemCarrito)
+    }
+}
+
+function eliminarItemCarrito(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+}
