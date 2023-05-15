efd236663483042a933ca2fd8f5a3bf258fb74c0
Miguel0o0 <127349178+Miguel0o0@users.noreply.github.com>
2023-05-15 00:15:50 -0400

    se arreglaron errores

--- js/carrito.js
+++ js/carrito.js
@@ -2,15 +2,14 @@ var carritoVisible = false
 
 if(Document.readyState=='loading'){
     document.addEventListener('DOMContentLoaded,ready')
-}
-else{
+}else{
     ready();
 }
 
 function ready(){
-    var botonesEliminar = document.getElementsByClassName('btn-eliminar');
-    for(vari=0; i < botonesEliminar.length;i++){
-        var buttom = botonesEliminar[i]
+    var botonesEliminarItem = document.getElementsByClassName('btn-eliminar');
+    for(var i=0; i < botonesEliminarItem.length;i++){
+        var buttom = botonesEliminarItem[i]
         buttom.addEventListener('click', eliminarItemCarrito)
     }
 }
