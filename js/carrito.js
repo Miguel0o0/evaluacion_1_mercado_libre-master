1b3cf7200e7de8feee91c671f1bccfdb18168ccb
Miguel0o0 <127349178+Miguel0o0@users.noreply.github.com>
2023-05-15 23:14:36 -0400

    se agregaron mas funcionalidades a la parte de js del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -17,4 +17,24 @@ function ready(){
 function eliminarItemCarrito(event){
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
+
+    actualizarTotalCarrito();
+}
+
+function actualizarTotalCarrito(){
+    var carritoContenedor = document.getElementsByClassName('carrito') [0];
+    var carritoItems = carritoContenedor.getElementsByClassName('carrito.item');
+    var total = 0;
+
+    for(var i=0; carritoItems.length;i++){
+        var item = carritoItems[i];
+        var precioElemento = item.getElementsByClassName('carrito-item-precio')[0];
+        console.log(precioElemento);
+    }
+
+    var precio = parseFloat(precioElemento.innerText.replace('$','').replace('.',''));
+    console.log(precio);
+    var cantidadItem = item.getElementsByClassName('carrito-item-cantidad')[0];
+    var cantidad = cantidadItem.value;
+    console.log(cantidad);
 }
