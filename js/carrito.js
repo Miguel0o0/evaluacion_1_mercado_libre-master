04b4512ca4ec1322e199e4e70f109fb89f9e8ac5
Miguel0o0 <127349178+Miguel0o0@users.noreply.github.com>
2023-05-16 20:35:17 -0400

    se siguen agregando funcionalidades en js a js y arreglando algun que otro detalle

--- js/carrito.js
+++ js/carrito.js
@@ -19,22 +19,41 @@ function eliminarItemCarrito(event){
     buttonClicked.parentElement.remove();
 
     actualizarTotalCarrito();
+
+    ocultarCarrito();
 }
 
+
 function actualizarTotalCarrito(){
-    var carritoContenedor = document.getElementsByClassName('carrito') [0];
-    var carritoItems = carritoContenedor.getElementsByClassName('carrito.item');
+    var carritoContenedor = document.getElementsByClassName('carrito')[0];
+    var carritoItems = carritoContenedor.getElementsByClassName('carrito-item');
     var total = 0;
 
-    for(var i=0; carritoItems.length;i++){
+    for(var i=0; i< carritoItems.length;i++){
         var item = carritoItems[i];
         var precioElemento = item.getElementsByClassName('carrito-item-precio')[0];
-        console.log(precioElemento);
+        var precio = parseFloat(precioElemento.innerText.replace('$','').replace('.',''));
+        var cantidadItem = item.getElementsByClassName('carrito-item-cantidad')[0];
+        console.log(precio);
+        var cantidad = cantidadItem.value;
+        total = total + (precio * cantidad);
     }
 
-    var precio = parseFloat(precioElemento.innerText.replace('$','').replace('.',''));
-    console.log(precio);
-    var cantidadItem = item.getElementsByClassName('carrito-item-cantidad')[0];
-    var cantidad = cantidadItem.value;
-    console.log(cantidad);
+    total = Math.round(total * 100)/100;
+
+    document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es");
+
+}
+
+function ocultarCarrito(){
+    var carritoItems = document.getElementsByClassName('carrito-item')[0];
+    if(carritoItems.childElementCount==0){
+        var carrito = document.getElementsByClassName('carrito')[0];
+        carrito.style.marginRight = '-100';
+        carrito.style.opacity = '0';
+        carritoVisible = false;
+
+        var item = document.getElementsByClassName('contenedor-items')[0];
+        item.style.width = '100%';
+    }
 }
