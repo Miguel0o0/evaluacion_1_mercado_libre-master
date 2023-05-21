b1c786d86c92619baede141ee2a5825f4fc574fc
Miguel0o0 <127349178+Miguel0o0@users.noreply.github.com>
2023-05-21 15:08:03 -0400

    se comenzo a crear el js del registro

---
+++ js/singUp.js
@@ -0,0 +1,7 @@
+const singUpForm = document.querySelector('#singUpForm')
+singUpForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const name = document.querySelector('#name')
+    const email = document.querySelector('#email')
+    const password = document.querySelector('#password')
+})
